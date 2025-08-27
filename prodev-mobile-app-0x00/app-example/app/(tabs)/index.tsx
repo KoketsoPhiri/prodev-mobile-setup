8a8f40c6ea46b8c13e04d8accd6ba67c89c5d5db
KoketsoPhiri <koketsophiri789@gmail.com>
2025-08-27 15:34:16 +0200

    sort

---
+++ prodev-mobile-app-0x00/app-example/app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+// app/(tabs)/index.tsx
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 24 }}>First App Created</Text>
+  
+    </View>
+  );
+}
