992e26fae05210b867b7c8e884f468a029b6d2ee
KoketsoPhiri <koketsophiri789@gmail.com>
2025-08-27 16:01:40 +0200

    text element 2

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -11,12 +11,12 @@ export default function App() {
         <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
         <Text style={styles.smallText}>ALX is awesome</Text>
       </View>
-      <View>
+    </View>
+     <View>
         <Text style={styles.largeText}>Typescript is great if you practice more</Text>
         <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
         <Text style={styles.smallText}>ALX is awesome</Text>
       </View>
-    </View>
     
   );
 }
